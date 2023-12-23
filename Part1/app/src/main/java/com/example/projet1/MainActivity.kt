101b8943c6d43454a7b7363a87574e3a34357db7
Sacha924 <sacha.simon@edu.devinci.fr>
2023-12-23 20:07:09 +0100

    add interactivity for google/facebook/apple button

--- Part1/app/src/main/java/com/example/projet1/MainActivity.kt
+++ Part1/app/src/main/java/com/example/projet1/MainActivity.kt
@@ -2,8 +2,22 @@ package com.example.projet1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    fun onSocialIconClicked(view: View) {
+        val service = when (view.id) {
+            R.id.google -> "Google"
+            R.id.facebook -> "Facebook"
+            R.id.apple -> "Apple ID"
+            else -> ""
+        }
+
+        val connectedTextView = findViewById<TextView>(R.id.connectedServiceTextView)
+        connectedTextView.text = "Connected with $service"
+        connectedTextView.visibility = View.VISIBLE // Rendez le TextView visible
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
